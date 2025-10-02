18d107014192ebd7d1c7bd3693222eabf8ed8418
Br0cod3 <thespammingbott@gmail.com>
2025-10-02 06:54:21 +0100

    implement: fibonnaciRec that calculates fibonacci sequence recursively

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 function fibonacci(n) {
     if (n < 1) return "Invalid"
-    if (n === 1) return [0]
-    if (n === 2) return [1]
     let arr = []
     let [previousNum, currentNum] = [0, 1]
     for ( let i = 0; i < n; i++) {
@@ -13,3 +11,11 @@ function fibonacci(n) {
     return arr
 }
 
+function fibonacciRec(n) {
+    if (n === 0) return [0]
+    if (n === 1) return [0, 1]
+
+    let fibs = fibonacciRec(n - 1)
+    fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2])
+    return fibs
+}
